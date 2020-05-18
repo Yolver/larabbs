3242cdab9416444fed4607a070708c3285ba249d
Yolver <735405853@qq.com>
2020-05-18 17:39:29 +0800

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -7,13 +7,18 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent"> <!-- Left Side Of Navbar -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
+
       </ul>
+
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="#">登录</a></li> <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
-      </ul> </div>
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+      </ul>
+    </div>
   </div>
 </nav>
