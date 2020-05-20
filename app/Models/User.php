0dae210893f9d2b37b3c76c66d5fb851c1197b0b
Yolver <735405853@qq.com>
2020-05-20 16:11:49 +0800

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,11 +19,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     public function notify($instance)
     {
-        if ($this->id == Auth::id()){
-            return ;
+        if ($this->id == Auth::id()) {
+            return;
         }
 
-        if (method_exists($instance, 'toDatabase')){
+        if (method_exists($instance, 'toDatabase')) {
             $this->increment('notification_count');
         }
 
@@ -36,7 +36,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar',
+        'name',
+        'email',
+        'password',
+        'introduction',
+        'avatar',
     ];
 
     /**
@@ -45,7 +49,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -77,5 +82,25 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+        $this->attributes['avatar'] = $path;
     }
 }
